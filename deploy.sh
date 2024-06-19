1b1082a759ace80d0c49166fa361e82116fc6715
kjhb45 <148983988+kjhb45@users.noreply.github.com>
2024-06-19 22:21:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git add -A
 
 git commit -m 'deploy'
 
-git push -f git@github.com:lin-snow/https://github.com/kjhb45/blog-file.git main#向存储库推送
+git push -f git@github.com:kjhb45/blog-file.git main#向存储库推送
